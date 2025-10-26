958849791b093c0807944ab88052d1a77204946f
Capfund <onions193@gmail.com>
2025-10-26 11:06:09 +0700

    Testing Indicator

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,6 @@ int light_mode = 1;
 
 void kernel_main(void) {
     vga_clear_screen();
-    vga_putstr("Welcome to BottleOS Shell [light] \n", color_green_on_black());
+    vga_putstr("Welcome to BottleOS Shell [light, testing branch] \n", color_green_on_black());
     shell_start();
 }
