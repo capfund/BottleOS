690497eaa0550ce1fd5b46da84bf9d5bf6c9365b
Capfund <onions193@gmail.com>
2025-11-30 16:22:51 +0700

    ps/2 mouse drivers

--- src/graphics/graphics.c
+++ src/graphics/graphics.c
@@ -1,5 +1,6 @@
 #include "graphics.h"
 #include "vga_driver.h"
+#include "../mouse/ps2.h"
 
 // Active driver
 static graphics_driver_t *graphics_active_driver = 0;
@@ -44,16 +45,59 @@ void graphics_draw_happy_face(int x, int y, graphics_color_t color) {
     graphics_plot_pixel(x + 10, y + 8, color);
 }
 
-void vga_test() {
-	graphics_set_driver(&graphics_vga_driver);
+void graphics_draw_cursor(int x, int y, graphics_color_t color) {
+    for (int i = -2; i <= 2; i++) {
+        graphics_plot_pixel(x + i, y, color);
+        graphics_plot_pixel(x, y + i, color);
+    }
+}
 
-    // graphic functionalitys
-    while (1) {
+void vga_test() {
+    graphics_set_driver(&graphics_vga_driver);
+
+    MousePacket pkt;
+    if (mouse_init() != 0) {
         graphics_clear_screen();
+        graphics_draw_rectangle(10, 50, 20, 20, 4); // error indicator
+        while (1);
+    }
+
+    // Initial positions
+    int mouse_x = 160;
+    int mouse_y = 100;
+    int scroll_bar_height = 20;
+
+    while (1) {
+        // Clear the screen once per frame
+        graphics_clear_screen();
+
+        // Draw static graphics
         graphics_draw_rectangle(150, 10, 100, 50, 3);
-        // draw some faces
         graphics_draw_happy_face(10,10,2);
         graphics_draw_happy_face(100,100,5);
-        graphics_draw_happy_face(300,150,5);
+        graphics_draw_happy_face(200,150,5);
+
+        // Poll all available mouse packets for this frame
+        while (mouse_poll(&pkt) == 1) {
+            mouse_x += pkt.dx;
+            mouse_y -= pkt.dy;
+
+            // Clamp to screen
+            if (mouse_x < 0) mouse_x = 0;
+            if (mouse_x > 319) mouse_x = 319;
+            if (mouse_y < 0) mouse_y = 0;
+            if (mouse_y > 199) mouse_y = 199;
+
+            // Scroll wheel visual
+            if (pkt.dz != 0) {
+                scroll_bar_height += pkt.dz * 2;
+                if (scroll_bar_height < 5) scroll_bar_height = 5;
+                if (scroll_bar_height > 50) scroll_bar_height = 50;
+            }
+        }
+
+        // Draw cursor and scroll bar once per frame
+        graphics_draw_cursor(mouse_x, mouse_y, 2);
+        graphics_draw_rectangle(300, 10, 20, scroll_bar_height, 5);
     }
 }
