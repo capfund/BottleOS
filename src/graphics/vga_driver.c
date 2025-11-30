2ac167960a0c1e38d845531f038febfa87fd6337
Capfund <onions193@gmail.com>
2025-11-30 17:54:19 +0700

    bugfix vga

--- src/graphics/vga_driver.c
+++ src/graphics/vga_driver.c
@@ -1,11 +1,11 @@
 #include "vga_driver.h"
 #include "../kernel.h"  // for ioport_in/out
 #include "../screen/screen.h"
-#include <string.h>
 
 #define VGA_ADDRESS 0xA0000
 
-// ---------- IOPORT helpers ----------
+// Helper start
+
 void ioport_out(unsigned short port, unsigned char val) {
     __asm__ __volatile__("outb %0, %1" : : "a"(val), "Nd"(port));
 }
@@ -16,87 +16,91 @@ unsigned char ioport_in(unsigned short port) {
     return ret;
 }
 
-// ---------- VGA register setup ----------
 void write_regs(unsigned char *regs)
 {
-    unsigned i;
+	unsigned i;
 
-    ioport_out(VGA_MISC_WRITE, *regs++);
-    for(i = 0; i < VGA_NUM_SEQ_REGS; i++) {
-        ioport_out(VGA_SEQ_INDEX, i);
-        ioport_out(VGA_SEQ_DATA, *regs++);
-    }
-
-    // Unlock CRTC registers
-    ioport_out(VGA_CRTC_INDEX, 0x03);
-    ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) | 0x80);
-    ioport_out(VGA_CRTC_INDEX, 0x11);
-    ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) & ~0x80);
-
-    regs[0x03] |= 0x80;
-    regs[0x11] &= ~0x80;
-
-    for(i = 0; i < VGA_NUM_CRTC_REGS; i++) {
-        ioport_out(VGA_CRTC_INDEX, i);
-        ioport_out(VGA_CRTC_DATA, *regs++);
-    }
-
-    for(i = 0; i < VGA_NUM_GC_REGS; i++) {
-        ioport_out(VGA_GC_INDEX, i);
-        ioport_out(VGA_GC_DATA, *regs++);
-    }
-
-    for(i = 0; i < VGA_NUM_AC_REGS; i++) {
-        (void)ioport_in(VGA_INSTAT_READ);
-        ioport_out(VGA_AC_INDEX, i);
-        ioport_out(VGA_AC_WRITE, *regs++);
-    }
-
-    (void)ioport_in(VGA_INSTAT_READ);
-    ioport_out(VGA_AC_INDEX, 0x20);
+/* write MISCELLANEOUS reg */
+	ioport_out(VGA_MISC_WRITE, *regs);
+	regs++;
+/* write SEQUENCER regs */
+	for(i = 0; i < VGA_NUM_SEQ_REGS; i++)
+	{
+		ioport_out(VGA_SEQ_INDEX, i);
+		ioport_out(VGA_SEQ_DATA, *regs);
+		regs++;
+	}
+/* unlock CRTC registers */
+	ioport_out(VGA_CRTC_INDEX, 0x03);
+	ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) | 0x80);
+	ioport_out(VGA_CRTC_INDEX, 0x11);
+	ioport_out(VGA_CRTC_DATA, ioport_in(VGA_CRTC_DATA) & ~0x80);
+/* make sure they remain unlocked */
+	regs[0x03] |= 0x80;
+	regs[0x11] &= ~0x80;
+/* write CRTC regs */
+	for(i = 0; i < VGA_NUM_CRTC_REGS; i++)
+	{
+		ioport_out(VGA_CRTC_INDEX, i);
+		ioport_out(VGA_CRTC_DATA, *regs);
+		regs++;
+	}
+/* write GRAPHICS CONTROLLER regs */
+	for(i = 0; i < VGA_NUM_GC_REGS; i++)
+	{
+		ioport_out(VGA_GC_INDEX, i);
+		ioport_out(VGA_GC_DATA, *regs);
+		regs++;
+	}
+/* write ATTRIBUTE CONTROLLER regs */
+	for(i = 0; i < VGA_NUM_AC_REGS; i++)
+	{
+		(void)ioport_in(VGA_INSTAT_READ);
+		ioport_out(VGA_AC_INDEX, i);
+		ioport_out(VGA_AC_WRITE, *regs);
+		regs++;
+	}
+/* lock 16-color palette and unblank display */
+	(void)ioport_in(VGA_INSTAT_READ);
+	ioport_out(VGA_AC_INDEX, 0x20);
 }
+// helper end
 
-// ---------- Mode 320x200x256 ----------
 static unsigned char g_320x200x256[] =
 {
-    0x63,                 // MISC
-    0x03,0x01,0x0F,0x00,0x0E,   // SEQ
-    0x5F,0x4F,0x50,0x82,0x54,0x80,0xBF,0x1F,
-    0x00,0x41,0x00,0x00,0x00,0x00,0x00,0x00,
-    0x9C,0x0E,0x8F,0x28,0x40,0x96,0xB9,0xA3,
-    0xFF,                  // CRTC
-    0x00,0x00,0x00,0x00,0x00,0x40,0x05,0x0F,
-    0xFF,                  // GC
-    0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
-    0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
-    0x41,0x00,0x0F,0x00,0x00 // AC
+/* MISC */
+	0x63,
+/* SEQ */
+	0x03, 0x01, 0x0F, 0x00, 0x0E,
+/* CRTC */
+	0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+	0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	0x9C, 0x0E, 0x8F, 0x28,	0x40, 0x96, 0xB9, 0xA3,
+	0xFF,
+/* GC */
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
+	0xFF,
+/* AC */
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+	0x41, 0x00, 0x0F, 0x00,	0x00
 };
 
-// ---------- Backbuffer ----------
-static unsigned char backbuffer[320*200];
-
-// ---------- VGA functions ----------
 static void vga_init() {
     write_regs(g_320x200x256);
-    memset(backbuffer, 0, sizeof(backbuffer));
-    // copy backbuffer to VGA once
-    unsigned char *VGA = (unsigned char*) VGA_ADDRESS;
-    memcpy(VGA, backbuffer, sizeof(backbuffer));
+    graphics_vga_driver.clear_screen(0); // default black
 }
 
 static void vga_clear_screen(graphics_color_t color) {
-    memset(backbuffer, color, sizeof(backbuffer));
+    unsigned char *VGA = (unsigned char*) VGA_ADDRESS;
+    for (int i = 0; i < 320 * 200; i++) {
+        VGA[i] = color;
+    }
 }
 
 static void vga_plot_pixel(int x, int y, graphics_color_t color) {
-    if (x < 0 || x >= 320 || y < 0 || y >= 200) return;
-    backbuffer[x + y * 320] = color;
-}
-
-// Flush backbuffer to VGA memory (call once per frame)
-static void vga_flush() {
     unsigned char *VGA = (unsigned char*) VGA_ADDRESS;
-    memcpy(VGA, backbuffer, sizeof(backbuffer));
+    VGA[x + y * 320] = color;
 }
 
 // VGA driver instance
@@ -105,8 +109,3 @@ graphics_driver_t graphics_vga_driver = {
     .clear_screen = vga_clear_screen,
     .plot_pixel = vga_plot_pixel
 };
-
-// Optional: expose flush so main loop can draw frame once
-void graphics_flush() {
-    vga_flush();
-}
